13acf6c739e4c6c3be5f0a92b6fb32d2b163b79e
Jeong Hwan Lee <54949453+JuicyJerry@users.noreply.github.com>
2023-08-17 16:02:59 +0900

    change index.js : port

--- server/index.js
+++ server/index.js
@@ -6,23 +6,23 @@ const hostname = "127.0.0.1";
 
 const { MONGO_URI, PORT } = config;
 
-let mongo_url = "";
-let port = 5001;
-if (process.env.NODE_ENV === "production") {
-  console.log("check ==> pro mode");
-  // mongo_url = process.env.MONGO_URI;
-  mongo_url = MONGO_URI;
-  port = process.env.PORT;
-} else {
-  console.log("check ==> dev mode");
-  port = PORT;
-  mongo_url = MONGO_URI;
-}
+// let mongo_url = "";
+// let port = 5001;
+// if (process.env.NODE_ENV === "production") {
+//   console.log("check ==> pro mode");
+//   // mongo_url = process.env.MONGO_URI;
+//   mongo_url = MONGO_URI;
+//   port = process.env.PORT;
+// } else {
+//   console.log("check ==> dev mode");
+//   port = PORT;
+//   mongo_url = MONGO_URI;
+// }
 
-console.log("index.js ==> ", mongo_url);
+console.log("index.js ==> ", MONGO_URI);
 // var db = mongoose.connection;
 const connect = mongoose
-  .connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log("error!!!!!!!!", err));
 
@@ -41,7 +41,7 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-console.log("index.js1 ==> ", port);
+// console.log("index.js1 ==> ", port);
 console.log("index.js2 ==> ", process.env.PORT);
 server.listen(process.env.PORT || 5001, hostname, () => {
   // server.listen(port || 5001, hostname, () => {
