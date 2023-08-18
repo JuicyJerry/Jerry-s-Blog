c9535a33ea3b1ebb300497f8ece565a398468ec8
Jeong Hwan Lee <54949453+JuicyJerry@users.noreply.github.com>
2023-08-18 11:24:41 +0900

    try to connect heroku

--- server/index.js
+++ server/index.js
@@ -4,32 +4,30 @@ var mongoose = require("mongoose");
 
 const hostname = "127.0.0.1";
 
-const { MONGO_URI, PORT } = config;
+// const { MONGO_URI, PORT } = config;
 
 let mongo_url = "";
 let port = "8080";
 if (process.env.NODE_ENV === "production") {
   console.log("check ==> pro mode");
   // mongo_url = process.env.MONGO_URI;
-  mongo_url = MONGO_URI;
+  mongo_url = process.env.MONGO_URI;
   port = process.env.PORT;
 } else {
   console.log("check ==> dev mode");
-  port = PORT;
-  mongo_url = MONGO_URI;
+  port = process.env.PORT;
+  mongo_url = process.env.MONGO_URI;
 }
 
-console.log("index.js ==> ", mongo_url);
+console.log("index.js1 ==> ", process.env.MONGO_URI);
+console.log("index.js2 ==> ", mongo_url);
 // var db = mongoose.connection;
 // const connect = mongoose
 //   .connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
 //   .then(() => console.log("MongoDB Connected..."))
 //   .catch((err) => console.log("error!!!!!!!!", err));
 mongoose
-  .connect(
-    mongo_url
-    // "mongodb+srv://ljh8842:2023Goal@cluster0.l7dbshf.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(mongo_url)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log("error!!!!!!!!", err));
 
@@ -48,7 +46,7 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-console.log("index.js ==> ", port);
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+console.log("index.js ==> ", process.env.PORT);
+server.listen(process.env.PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${process.env.PORT}/`);
 });
